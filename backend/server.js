433a357574c5795bf0a1e396809322a88320c478
sayasam132 <132969889+sayasam132@users.noreply.github.com>
2026-03-22 19:15:58 -0600

    arreglar CORS OPTIONS

--- backend/server.js
+++ backend/server.js
@@ -3,8 +3,9 @@ require('dotenv').config();
 
 const authRoutes = require('./routes/auth.routes');
 const canchasRoutes = require('./routes/canchas.routes');
-const reservasRoutes = require('./routes/reservas.routes')
-const reportesRoutes = require('./routes/reportes.routes')
+const reservasRoutes = require('./routes/reservas.routes');
+const reportesRoutes = require('./routes/reportes.routes');
+const pagosRoutes = require('./routes/pagos.routes');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -13,7 +14,10 @@ app.use(express.json());
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
   next();
 });
 
@@ -21,6 +25,7 @@ app.use('/api/auth', authRoutes);
 app.use('/api/canchas', canchasRoutes);
 app.use('/api/reservas', reservasRoutes);
 app.use('/api/reportes', reportesRoutes);
+app.use('/api/pagos', pagosRoutes);
 
 app.get('/', (req, res) => {
   res.json({ message: 'Servidor de Canchas App funcionando ✅' });
