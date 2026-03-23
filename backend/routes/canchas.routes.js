9e9b965f3386cad39a6fbbc88aa1f1c71badb3d6
sayasam132 <132969889+sayasam132@users.noreply.github.com>
2026-03-22 22:13:55 -0600

    agregar endpoint horas ocupadas

--- backend/routes/canchas.routes.js
+++ backend/routes/canchas.routes.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const router = express.Router()
-const { getCanchas, getCancha } = require('../controllers/canchas.controller')
+const { getCanchas, getCancha, getHorasOcupadas } = require('../controllers/canchas.controller')
 
 router.get('/', getCanchas)
 router.get('/:id', getCancha)
+router.get('/:id/horas-ocupadas', getHorasOcupadas)
 
 module.exports = router
