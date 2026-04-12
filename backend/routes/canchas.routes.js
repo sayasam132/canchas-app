5fe714ab9e94603c3c44ffccf095e18125b3be10
sayasam132 <132969889+sayasam132@users.noreply.github.com>
2026-04-12 15:10:59 -0600

    quitar auth de ruta todas canchas

--- backend/routes/canchas.routes.js
+++ backend/routes/canchas.routes.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const router = express.Router()
-const { getCanchas, getCancha, getHorasOcupadas, crearCancha, editarCancha, eliminarCancha } = require('../controllers/canchas.controller')
+const { getCanchas, getCancha, getHorasOcupadas, crearCancha, editarCancha, eliminarCancha, getTodasCanchas } = require('../controllers/canchas.controller')
 const authMiddleware = require('../middleware/auth.middleware')
 
+router.get('/todas', getTodasCanchas)
 router.get('/', getCanchas)
 router.get('/:id', getCancha)
 router.get('/:id/horas-ocupadas', getHorasOcupadas)
