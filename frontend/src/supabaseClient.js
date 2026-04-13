029aaa3da7e39156699a0d03cd6b13d9e0b81b62
sayasam132 <132969889+sayasam132@users.noreply.github.com>
2026-04-12 18:05:40 -0600

    arreglar supabaseClient encoding

---
+++ frontend/src/supabaseClient.js
@@ -0,0 +1,4 @@
+﻿import { createClient } from '@supabase/supabase-js'
+const supabaseUrl = 'https://toiafoxboonmacowjggb.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRvaWFmb3hib29ubWFjb3dqZ2diIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI5Mjk3MzQsImV4cCI6MjA4ODUwNTczNH0.fjcSaEioY4qGlna2Fgf1B5py4rZZX6Ec7RJVM5i7Qhg'
+export const supabase = createClient(supabaseUrl, supabaseKey)
